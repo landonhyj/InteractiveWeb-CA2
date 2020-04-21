dbb43177ec9a29b336d556b95e5b1c5960307e26
yanjuehau <landonhyj@gmail.com>
2020-04-21 13:25:24 +0000

    added some require modules

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ cors = require("cors"),
 http = require("http"),
 express = require("express"),
 bodyParser = require("body-parser"),
+xml2js =require('xml2js'),//This module does XML to JSON conversion and also allows us to get from JSON back to XML
+express = require('express'),//This module allows this app to respond to HTTP Requests, defines the routing and renders back the required content
+fs = require('fs'),//This module allows to work witht the file system: read and write files back
+xmlParse = require('xslt-processor').xmlParse,//This module allows us to work with XML files
+xsltProcess = require('xslt-processor').xsltProcess,//The same module allows us to utilise XSL Transformations
 mongoose = require('mongoose');
 
 var app = express();
